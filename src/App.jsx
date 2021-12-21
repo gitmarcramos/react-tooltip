8d5cd7bf19d652967541be155f751d40b889e228
Marc Ramos <marcramos.webdev@gmail.com>
2021-12-21 12:58:21 +0100

    tooltip for input or other elements (buttons, icons etc.) handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.css";
 import Tooltip from "./Components/Tooltip/Tooltip";
+import SVG from "./../src/infos.svg";
 
 function App() {
   return (
@@ -7,18 +8,26 @@ function App() {
       <h1>React Tooltips</h1>
       <div className="main-container">
         <div className="container">
-          <Tooltip content="This button can be hovered!" position="left" status="inactive">
+          <Tooltip content="A toolip contains usefull information about an element" position="top" type="">
             <button>Hover me</button>
           </Tooltip>
         </div>
         <div className="container">
-          <div className="input-wrapper">
-            <label htmlFor="input">Hover me again!</label>
-            <input type="text" id="input" />
-          </div>
+          <Tooltip
+            content="This one is really long! Its purpose is only to show an extra big tooltip, so the content doesn't really matter... Well, actually, it does a bit, as long as its large. So, if you read all this, how you doin? "
+            position="top"
+            type="input"
+          >
+            <div className="input-wrapper">
+              <label htmlFor="input">Click on the input</label>
+              <input type="text" id="input" />
+            </div>
+          </Tooltip>
         </div>
         <div className="container">
-          <h2>This is some lorem ipsum text that can be hovered</h2>
+          <Tooltip content="Tooltip" position="top" type="button">
+            <img src={SVG} className="info-tooltip" alt="info tooltip icon" />
+          </Tooltip>
         </div>
       </div>
     </div>
--- src/Components/Tooltip/Tooltip.jsx
+++ src/Components/Tooltip/Tooltip.jsx
@@ -1,36 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Tooltip.css";
 
 export default function Tooltip(props) {
-  // Tooltip state
-  const [tooltip, setTooltip] = useState(props);
+  //Make tooltip appear on hover OR focus
+  const [status, setStatus] = useState(true);
+
+  const handleEvents = () => {
+    setStatus(!status);
+  };
 
   // Set position of the tooltip
   const [position, setPosition] = useState(props.position);
   switch (position) {
     case "top":
-      setPosition({ top: "-100%" });
+      setPosition({ bottom: "calc(100% + 15px)" });
       break;
     case "bottom":
-      setPosition({ bottom: "-100%" });
+      setPosition({ top: "calc(100% + 15px)" });
       break;
     case "right":
-      setPosition({ left: "50%", transform: "translateX(50%)" });
+      setPosition({ left: "50%", transform: "translateX(100%)" });
       break;
     case "left":
       setPosition({ right: "50%", transform: "translateX(-50%)" });
   }
 
-  //Make tooltip appear on hover
-
   return (
     <div className="tooltip">
       {/* Render the element to use tooltip with */}
-      <div className="element">{props.children}</div>
-
+      {props.type === "input" ? (
+        <div className="element" onFocus={handleEvents} onBlur={handleEvents}>
+          {props.children}
+        </div>
+      ) : (
+        <div
+          className="element"
+          onMouseEnter={handleEvents}
+          onMouseLeave={handleEvents}
+        >
+          {props.children}
+        </div>
+      )}
       {/* Tooltip */}
-      <div className="tooltip-main" style={position}>
-        {props.content}{" "}
+      <div
+        className={status ? "tooltip-main tooltip-hidden" : "tooltip-main"}
+        style={position}
+      >
+        {props.content}
       </div>
     </div>
   );
